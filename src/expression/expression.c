f2b7a7b57fdbb08cdbef2cbd148c994814df7592
BorisBug <69357420+BorisBug@users.noreply.github.com>
2021-12-09 01:56:18 +0100

    expression

--- src/expression/expression.c
+++ src/expression/expression.c
@@ -5,5 +5,30 @@ bool expression_check(const char *exp)
 {
     bool status = false;
 
+    if(exp)
+    {
+        status = true;
+        stack_clear();
+
+        while(*exp)
+        {
+            if(*exp==')')
+            {
+                status = stack_pop() == '(';
+            }
+            else
+            if(*exp=='(')
+            {
+                status = false;
+                (void)stack_push('(');
+            }
+
+            exp++;
+        }
+
+        if(status)
+            status = stack_pop() == 0;
+    }
+    
     return status;
 }
